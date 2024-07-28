d2263a781cef986c09fa22c8128d5f766806cb8f
Uunkn0wnN <95006878+Uunkn0wnN@users.noreply.github.com>
2024-07-28 18:36:12 +0700

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -42,10 +42,10 @@ const updateNavbarContent = () => {
     if (hashAndLinkedLinks[window.location.hash]) {
         if(window.location.hash !== '#home') hashAndLinkedLinks[window.location.hash].classList.add('active-link')
         document.getElementById('current-location').innerText = window.location.hash.substring(1)
-        document.title = `Bαɠαѕ | ${window.location.hash.substring(1)}`
+        document.title = `yarz | ${window.location.hash.substring(1)}`
     } else {
         document.getElementById('current-location').innerText = '404'
-        document.title = `Bαɠαѕ | ${404}`
+        document.title = `yarz | ${404}`
     }
     
     typeNavbar()
